bb4eead976f11982f873ef623638b2ebf8b1f081
HarvMatthews <Harveymjm15@gmail.com>
2025-02-20 16:17:14 +0000

    Init commit of acitivity class

---
+++ Backend/src/RockRouteAPI/Classes/Activity.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace RockRoute.Classes
+{
+    public class Activity
+    {
+
+        private string _name;
+        private DateTime? _date;
+        private string _notes;
+
+
+        public string Name
+        {
+            get => _name;
+            set { _name;}
+        }
+
+        public DateTime? Date 
+        {
+            get => _date;
+            set { _date;}
+        }
+
+        public string Notes 
+        {
+            get => _notes:
+            set { _notes;}
+        }
+
+        public 
+
+        public Activity(string Name, DateTime Date, Notes) {
+            this._date = Date
+            this._name = Name
+            this._notes = Notes
+        }
+
+
+
+    }
+}
