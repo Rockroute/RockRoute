0e6b925c49e2a343fa0da942d0789e4b4b60c93b
HarvMatthews <Harveymjm15@gmail.com>
2025-02-21 12:38:10 +0000

    Logbook? except FK/PK

--- Backend/src/RockRouteAPI/Models/LogBook.cs
+++ Backend/src/RockRouteAPI/Models/LogBook.cs
@@ -1,21 +1,16 @@
 using system;
 using System.Data.Entity;
-using RockRoute.class
-{
-    
-}
+using RockRoute.classes;
 
 namespace RockRoute.Models //accessible from other areas of the project 
 {
     public class LogBook //Defining a user
     {
-        [ForeignKey(UserID)]
-        public string UserID {get; set;}
-        [ForeignKey(RouteID)]
-        public string RouteID {get; set;}
-        public <Playlist> Playlist  {get; set;}
-        public <Route> Route  {get; set;}
-        public  {get; set;}
+        public string UserID { get; set; } //FK
+        public string RouteID { get; set; } //FK
+        public List<Playlist> Playlist {get; set;} //List of type Playlist defined in class
+        
+        public List<Route> Route  {get; set;}
+        public List<Activity> Activity { get; set; }
     }
-
 }
