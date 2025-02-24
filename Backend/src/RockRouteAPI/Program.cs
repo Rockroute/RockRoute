741507d7bb644515fb993b0bbddd1d1b2f743222
Peter Hamilton <sgphamil@liverpool.ac.uk>
2025-02-24 13:52:53 +0000

    Added Required code in Program.cs, memoryDBs in use for testing for now

--- Backend/src/RockRouteAPI/Program.cs
+++ Backend/src/RockRouteAPI/Program.cs
@@ -1,41 +1,29 @@
-var builder = WebApplication.CreateBuilder(args);
+using RockRoute.Models;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+namespace RockRoute {
+    var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+    // Add services to the container.
+    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    builder.Services.AddOpenApi();
+    builder.Services.AddDbContext<ClimbsDB>(opt =>
+        opt.UseInMemoryDatabase("ClimbsDB"));
+    builder.Services.AddDbContext<LogBooksDB>(opt =>
+        opt.UseInMemoryDatabase("LogBooksDB"));
+    builder.Services.AddDbContext<UsersDB>(opt =>
+        opt.UseInMemoryDatabase("UsersDB"));
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+    var app = builder.Build();
 
-app.UseHttpsRedirection();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
 }
