42769da52b8764118032b7f1795cc6bd34e28043
Peter Hamilton <kittyrhamilton@gmail.com>
2025-03-31 10:17:30 +0100

    Fixed API Calls
    Needs to be checked later, Arch system has just been murdered by ICU being updated to V76... should've hopped on the forums before forcing an update 🙄

--- Frontend/src/RockRoute/ApiCalling/ApiCallTest.cs
+++ Frontend/src/RockRoute/ApiCalling/ApiCallTest.cs
@@ -3,8 +3,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using System.Collections.Generic; //to use List
-
+using System.Collections.Generic;
 
 using RockRoute.Classes;
 using RockRoute.Models;
@@ -12,27 +11,34 @@ using RockRoute.enums;
 
 namespace RockRoute.ApiCall
 {
-
-
     class APIprogram
     {
+        private static readonly string _baseAPIUrl = "http://localhost:5297/";
 
-        
-        static HttpClient client = new HttpClient();
+        static HttpClientHandler handler = new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+        };
+
+        static HttpClient client = new HttpClient(handler);
+
+        static APIprogram()
+        {
+            client.BaseAddress = new Uri(_baseAPIUrl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
 
         static void ShowClimb(Climb climb)
         {
-            Console.WriteLine($"Name: {climb.RouteName}\tPrice: " +
-                $"{climb.RouteName}\tCategory: {climb.RouteName}");
+            Console.WriteLine($"Name: {climb.RouteName}\tPrice: {climb.RouteName}\tCategory: {climb.RouteName}");
         }
 
         static async Task<Uri> CreateClimbAsync(Climb climb)
         {
-            HttpResponseMessage response = await client.PostAsJsonAsync(
-                "api/ClimbsDB", climb);
+            HttpResponseMessage response = await client.PostAsJsonAsync("api/ClimbsDB", climb);
             response.EnsureSuccessStatusCode();
-
-            // return URI of the created resource.
             return response.Headers.Location;
         }
 
@@ -49,42 +55,26 @@ namespace RockRoute.ApiCall
 
         static async Task<Climb> UpdateClimbAsync(Climb climb)
         {
-            HttpResponseMessage response = await client.PutAsJsonAsync(
-                $"api/ClimbsDB/{climb.RouteId}", climb);
+            HttpResponseMessage response = await client.PutAsJsonAsync($"api/ClimbsDB/{climb.RouteId}", climb);
             response.EnsureSuccessStatusCode();
-
-            // Deserialize the updated climb from the response body.
             climb = await response.Content.ReadAsAsync<Climb>();
             return climb;
         }
 
         static async Task<HttpStatusCode> DeleteClimbAsync(string routeId)
         {
-            HttpResponseMessage response = await client.DeleteAsync(
-                $"api/ClimbsDB/{routeId}");
+            HttpResponseMessage response = await client.DeleteAsync($"api/ClimbsDB/{routeId}");
             return response.StatusCode;
         }
 
-        /*static void Main()
-        {
-            RunAsync().GetAwaiter().GetResult();
-        }
-*/
         public static async Task RunAsync()
         {
-            // Update port # in the following line.
-            client.BaseAddress = new Uri("http://localhost:5297");
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-
             try
             {
-                // Create a new climb
                 Climb climb = new Climb
                 {
                     RouteName = "TheRouteName",
-                    RouteId = "LSOSAY", //No brackets in RouteID?
+                    RouteId = "LSOSAY",
                     SectorId = "djs8d",
                     ParentSector = "ClimbSector",
                     Type = climbTypes.Boulder,
@@ -98,23 +88,8 @@ namespace RockRoute.ApiCall
                 var url = await CreateClimbAsync(climb);
                 Console.WriteLine($"Created at {url}");
 
-                // Get the climb
                 climb = await GetClimbAsync(url.PathAndQuery);
                 ShowClimb(climb);
-
-                // Update the climb
-                //Console.WriteLine("Updating price...");
-                //climb.RouteName = "NEWNAME";
-                //await UpdateClimbAsync(climb);
-
-                // Get the updated climb
-                //climb = await GetClimbAsync(url.PathAndQuery);
-                //ShowClimb(climb);
-
-                // Delete the climb
-                //var statusCode = await DeleteClimbAsync(climb.RouteId);
-                //Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
-
             }
             catch (Exception e)
             {
