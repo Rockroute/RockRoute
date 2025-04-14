48a3b33816c97173e965923d3a223e60347043e4
HarvMatthews <Harveymjm15@gmail.com>
2025-04-14 12:06:39 +0100

    Create account working

--- Frontend/src/RockRoute/Views/CreateAccount.axaml.cs
+++ Frontend/src/RockRoute/Views/CreateAccount.axaml.cs
@@ -9,8 +9,9 @@ using RockRoute.Models;
 using RockRoute.Classes;
 using RockRoute.Helper;
 using RockRoute.enums;
+using System.Threading.Tasks;
 
-namespace RockRoute.Views 
+namespace RockRoute.Views
 {
     public partial class CreateAccount : Window
     {
@@ -19,32 +20,52 @@ namespace RockRoute.Views
             InitializeComponent();
         }
 
-        private void InitializeComponent() {
-            AvaloniaXamlLoader.Load(this);
-        }
-
-        private void GotAccountButton(object? sender, Avalonia.Interactivity.RoutedEventArgs e) {
-            var NewWindow = new Login();
-           //NewWindow.WindowState = WindowState.Maximized; //Uncomment this, This is just so i need minimise all the time to see debugger
-            NewWindow.Show();
-            this.Close();
-        }
-
-        private void CreateAccountButtonClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e) 
+        private void InitializeComponent()
         {
-            //I dont know how you get the Writing from Textbox, But just fill in the function below:
-            LoginFunctions.CreateAccountFunc("NEWUSER?A?!?!?", "NerUsersEmail", "newUsersPassword", "newUsersPassword");
-            //System.Console.WriteLine(LoginFunctions.CreateAccount("NEWUSER?A?!?!?", "NerUsersEmail", "newUsersPassword", "newUsersPassword"));
+            AvaloniaXamlLoader.Load(this);
 
-            var NewWindow = new MainWindow();
+            //Linking the textboxes across:
+            NameBox = this.FindControl<TextBox>("NameBox");
+            EmailBox = this.FindControl<TextBox>("EmailBox");
+            PasswordBox = this.FindControl<TextBox>("PasswordBox");
+            CPasswordBox = this.FindControl<TextBox>("CPasswordBox");
+
+        }
+
+        private void GotAccountButton(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+
+            //This is all okay and works
+            var NewWindow = new Login();
             if (!Program.DebugMode)
             {
                 NewWindow.WindowState = WindowState.Maximized; //Uncomment this, This is just so i need minimise all the time to see debugger
             }
-           
-           //NewWindow.WindowState = WindowState.Maximized; //Uncomment this, This is just so i need minimise all the time to see debugger
             NewWindow.Show();
             this.Close();
+        }
+
+        private async void CreateAccountButtonClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            login_Status createAccountStatus = await LoginFunctions.CreateAccountFunc(NameBox.Text, EmailBox.Text, PasswordBox.Text, CPasswordBox.Text);
+            System.Console.WriteLine(createAccountStatus);
+
+            if (createAccountStatus == login_Status.Account_Created)
+            {
+                //if account created succesfully, then login:
+
+                var NewWindow = new MainWindow();
+                if (!Program.DebugMode) //Debug mode
+                {
+                    NewWindow.WindowState = WindowState.Maximized; //Uncomment this, This is just so i need minimise all the time to see debugger
+                }
+
+                //NewWindow.WindowState = WindowState.Maximized; //Uncomment this, This is just so i need minimise all the time to see debugger
+                NewWindow.Show();
+                this.Close();
+            }
+
+
         }
     }
 }
