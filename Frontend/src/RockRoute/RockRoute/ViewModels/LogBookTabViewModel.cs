1f48a0e055516e9f428b8614aa48b0da3134d329
D0nOde <113692102+D0nOde@users.noreply.github.com>
2025-04-25 13:46:18 +0100

    LogBookTabViewModel change

--- Frontend/src/RockRoute/RockRoute/ViewModels/LogBookTabViewModel.cs
+++ Frontend/src/RockRoute/RockRoute/ViewModels/LogBookTabViewModel.cs
@@ -8,18 +8,46 @@ namespace RockRoute.ViewModels
     public class LogBookTabViewModel
     {
         public ObservableCollection<Playlist> playlists {get;} = new(); 
+        public ObservableCollection<string> colabIDs {get;} = new(); 
 
         public LogBookTabViewModel() 
         {
             LoadPlaylists();
         }
 
+        public List<string> Test(List<string> list) {
+            list.Add("c");
+            list.Add("c");
+            list.Add("c");
+            list.Add("c");
+            return list;
+        }
+
         public void LoadPlaylists() {
-            playlists.Add(new Playlist("test playlist","testCreator",new List<string>(),new List<string>(),null)
+            playlists.Add(new Playlist("test playlist","testCreator",new List<string>(),Test(new List<string>()),null)
             {
                 Name = "test-playlist",
                 CreatorID = "testID"
             });
+            playlists.Add(new Playlist("test playlist","testCreator",new List<string>(),Test(new List<string>()),null)
+            {
+                Name = "test-playlist",
+                CreatorID = "testID"
+            });
+        }
+
+        public void LoadcolabIDs() {
+            colabIDs.Add("");
+        }
+
+        public void AddCollaborator() {
+            colabIDs.Add("");
+        }
+
+        public void RemoveCollaborator() {
+            if (colabIDs.Count >= 1){
+                colabIDs.RemoveAt(colabIDs.Count - 1);
+            }
         }
     }
 }
