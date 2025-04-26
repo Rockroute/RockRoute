f16ffecd39a2dcb664e7ead1adb67222c345bd7d
LoveStruck20 <124519922+LoveStruck20@users.noreply.github.com>
2025-04-26 14:20:55 +0100

    Update HomeTab.axaml.cs
    Signed-off-by: LoveStruck20 <124519922+LoveStruck20@users.noreply.github.com>

--- Frontend/src/RockRoute/RockRoute/Views/HomeTab.axaml.cs
+++ Frontend/src/RockRoute/RockRoute/Views/HomeTab.axaml.cs
@@ -15,4 +15,15 @@ public partial class HomeTab : UserControl
         var homeTabViewModel = (HomeTabViewModel)this.DataContext;
         homeTabViewModel.SeeMore();
     }
+
+    public void LogOutButtonClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e) {
+            var window = this.GetVisualRoot() as Window;
+            var newWindow = new Login();
+            Program.loggedInUser.UserId = "NOT_LOGGED_IN";
+            Program.loggedInUser.Name = "NOT_LOGGED_IN";
+            Program.loggedInUser.Email = "NOT_LOGGED_IN";
+            Program.loggedInUser.Password = "NOT_LOGGED_IN";
+            newWindow.Show();
+            window?.Close();
+        }
 }
