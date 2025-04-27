2e6d2ceabdc00fee161d20d41ac3272b872bbb18
Kitt Hamilton <sgphamil@liverpool.ac.uk>
2025-04-27 19:54:30 +0100

    Updated homepage

--- Frontend/src/RockRoute/RockRoute/Views/HomeTab.axaml.cs
+++ Frontend/src/RockRoute/RockRoute/Views/HomeTab.axaml.cs
@@ -11,11 +11,7 @@ public partial class HomeTab : UserControl
         InitializeComponent();
     }
     // will call the SeeMore from the HomeTabViewModel class that will edit the recommendations by adding more
-    public void SeeMoreButton(object? sender, Avalonia.Interactivity.RoutedEventArgs e) {
-        
-        var homeTabViewModel = (HomeTabViewModel)this.DataContext;
-        homeTabViewModel.SeeMore();
-    }
+
 
     public void LogOutButtonClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e) {
 
